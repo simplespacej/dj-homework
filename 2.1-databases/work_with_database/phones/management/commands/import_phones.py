1c7581e869d4ea404968ef86278d0c01fa9a10fe
simplespacej <my-ptogramming@mail.ru>
2024-02-20 14:27:25 +0300

    Complete third task

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,26 @@
+# import_phones.py
 import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
+from datetime import datetime
 
 class Command(BaseCommand):
+    help = 'Import phones from a CSV file into the Phone model'
+
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('csv_file_path', type=str, help='The path to the CSV file to import.')
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
-            phones = list(csv.DictReader(file, delimiter=';'))
-
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        with open(options['csv_file_path'], newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=';')
+            for row in reader:
+                phone = Phone(
+                    id=row['id'],
+                    name=row['name'],
+                    price=row['price'],
+                    image=row['image'],
+                    release_date=datetime.strptime(row['release_date'], '%Y-%m-%d').date(),
+                    lte_exists=row['lte_exists'] == 'True',
+                )
+                phone.save()
+                self.stdout.write(self.style.SUCCESS(f"Phone '{phone.name}' imported successfully."))
