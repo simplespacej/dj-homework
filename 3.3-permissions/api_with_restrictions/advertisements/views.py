12ab4eb9b0d0181b15b40c150dc31a318c0da40c
simplespacej <my-ptogramming@mail.ru>
2024-03-21 13:46:18 +0300

    Complete seventh task

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,27 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .permissions import IsOwnerOrReadOnly  # Импортируйте ваш класс разрешений здесь
 
 class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
 
     def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
-        return []
+        """
+        Предоставляет различные разрешения в зависимости от типа запроса.
+        """
+        if self.action in ["create"]:
+            permission_classes = [IsAuthenticated]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            permission_classes = [IsOwnerOrReadOnly]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        """
+        Автоматически присваивает текущего пользователя в качестве создателя объявления.
+        """
+        serializer.save(creator=self.request.user)
