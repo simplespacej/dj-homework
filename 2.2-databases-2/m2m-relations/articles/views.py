6196b8e7f743bd804c9f2c15963dc0cd92ac629f
simplespacej <my-ptogramming@mail.ru>
2024-03-23 19:28:30 +0300

    Redone fourth task

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-
 from articles.models import Article
-
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
-
+    articles = Article.objects.prefetch_related('tags').order_by('-published_at')
+    context = {'articles': articles}
     return render(request, template, context)
